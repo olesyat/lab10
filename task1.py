eb5067107d443cf281b19397515209a69f4f2058
yarkalyba <lyba@ucu.edu.edu>
2018-04-21 00:10:01 +0300

    added task1.py changes in ecoo.py (added methods act and interaction to the class River)

--- task1.py
+++ task1.py
@@ -7,14 +7,14 @@ class River1(River):
     def __init__(self):
         super().__init__()
 
-
     def fight(self, animal1, animal2):
         if animal1 == None:
             return [str(animal2)]
         elif animal2 == None:
             return [str(animal1)]
         elif animal1 == animal2:
-            Ecosystem.BUFFER.append(animal2)
+            for i in range(2):
+                Ecosystem.BUFFER.append(animal2)
             return [str(animal1)]
         elif animal1 != animal2:
             return [str(Bear())]
