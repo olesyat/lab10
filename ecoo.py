3fdca5a1fb9dc20646fa8dc705652da2fc080555
olesyat <tretyak.olesya.lviv@gmail.com>
2018-04-20 12:56:03 +0300

    :)

--- ecoo.py
+++ ecoo.py
@@ -1,29 +1,22 @@
 import random
+import copy
+
 
 class Ecosystem:
     def __init__(self, river):
         self.river = river
 
-    def move(self):
-        pass
 
-
-class River(list):
-    def __init__(self):
-        self.size = random.randint(1, 100)
-        self.choices = [Bear(), Fish(), None]
-        for i in range(self.size):
-            self.append([random.choice(self.choices)])
-
-    def pr(self):
-        return self
 
 class Animal:
     def __init__(self):
         self._power = random.randint(1, 10)
         self._age = random.randint(1, 10)
-        self._sex = random.choice(True, False)
+        self._sex = random.choice([True, False])
         self._birth = self.KIDS
+
+    def __str__(self):
+        return type(self).__name__
 
 class Bear(Animal):
     KIDS = 2
@@ -35,5 +28,39 @@ class Otter(Animal):
     KIDS = 3
 
 
-a = River()
-print(a.pr)
+
+class River(list):
+    def __init__(self):
+        self.size = random.randint(1, 10)
+        self.choices = self.CHOICES
+        for i in range(self.size):
+            self.append([random.choice(self.choices)])
+
+    def move(self):
+        # new_river = copy.deepcopy(self)
+        # moves = [-1, 0, 1]
+        # print(new_river)
+        # for i in range(self.size):
+        #     a = random.choice(moves)
+        #     if len(new_river[i]) > 1:
+        #         new_river[i % self.size + a].append(new_river[i][0])
+        #     else:
+        #         new_river[i % self.size + a].append(new_river[i])
+        #     print("moving:", a, ",step", i, new_river)
+        #
+        # return new_river
+        new_river = [[] for i in range(self.size)]
+
+        return new_river
+
+
+class River_1(River):
+    CHOICES = [str(Bear()), str(Fish()), None]
+
+
+class River_3(River):
+    CHOICES = [Bear(), Fish(), Otter(), None]
+
+
+a = River_1()
+print(a.move())
